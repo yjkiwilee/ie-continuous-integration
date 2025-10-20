1e22620a9b4a771e0c7f0cfaf6c5922084f00990
Young Jun Lee <youngjun.lee@reuben.ox.ac.uk>
2025-10-20 11:48:41 +0100

    Configure autodocs

--- docs/conf.py
+++ docs/conf.py
@@ -11,15 +11,19 @@ copyright = '2025, Young Jun Lee'
 author = 'Young Jun Lee'
 release = '0.0.0'
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
--- docs/index.rst
+++ docs/index.rst
@@ -1,15 +1,10 @@
-.. cont-integration documentation master file, created by
-   sphinx-quickstart on Mon Oct 20 11:15:43 2025.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 cont-integration documentation
 ==============================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
+This is the documentation file for cont-integration.
 
+.. automodule:: ci_course.functionality
+   :members:
 
 .. toctree::
    :maxdepth: 2
